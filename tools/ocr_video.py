e68e1f1da1cf452e122d040e27d44810a1aa9b3b
unt2tled <99178443+unt2tled@users.noreply.github.com>
2022-07-30 15:40:29 +0300

    Minor change

--- tools/ocr_video.py
+++ tools/ocr_video.py
@@ -5,7 +5,7 @@ import shutil
 import difflib
 
 FRAMES_PATH = "tmp_frames"
-CONF_THRESH = 0.6
+CONF_THRESH = 0.9
 SIMILARITY_THRESH = 0.8
 
 def generate_frames(video_path, rate, show_print = True):
@@ -34,9 +34,9 @@ def add_text(text_lst, text):
           return
     text_lst.append(text)
 
-def retrieve_text(video_path, rate=5):
+def retrieve_text(video_path, rate = 5, show_print = True):
     texts_lst = []
-    generate_frames(video_path, rate = rate)
+    generate_frames(video_path, rate = rate, show_print = show_print)
     ocr = easyocr.Reader(['en'])
     for i in os.listdir(FRAMES_PATH):
         text = ocr.readtext(FRAMES_PATH + "/" + i)
@@ -48,3 +48,13 @@ def retrieve_text(video_path, rate=5):
     # Delete temporary directory
     shutil.rmtree(FRAMES_PATH)
     return texts_lst
+
+def retrieve_to_file(dest, video_path):
+  text_lst = retrieve_text(video_path, rate = 2, show_print = False)
+  file = open(dest, "w")
+  file.writelines([line + "\n" for line in text_lst])
+  file.close()
+
+def retrieve_to_files(dest, video_path):
+  for file_name in os.listdir(video_path):
+      retrieve_to_file(dest + "/" + os.path.splitext(file_name)[0] + "_text.txt", video_path + "/" + file_name)
