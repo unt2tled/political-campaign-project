3630dd67bda15a3c3bae7482a8f53616afa066dd
unt2tled <99178443+unt2tled@users.noreply.github.com>
2022-10-24 17:44:35 +0300

    Update table_updater.py

--- tools/table_updater.py
+++ tools/table_updater.py
@@ -2,23 +2,40 @@ from tempfile import NamedTemporaryFile
 import shutil
 import csv
 from colors_feature import retrieve_colors_from_video, retrieve_colors_per_frame
+import os
 
-filename = "tagging_db1.csv"
-tempfile = NamedTemporaryFile(mode="w", delete=False, newline="")
+VIDEOS_PATH = "C:\\Users\\un.t1tled\\Desktop\\videos\\"
 
-with open(filename, "r") as csvfile, tempfile:
-    reader = csv.DictReader(csvfile)
-    fields = reader.fieldnames
-    writer = csv.DictWriter(tempfile, fieldnames=fields)
-    writer.writeheader()
-    counter = 0
-    for row in reader:
-        counter += 1
-        print(counter)
-        name = row["name"]
-        path = "C:\\Users\\un.t1tled\\Desktop\\videos\\"+name+".wmv"
-        #row["color_total"] = str(retrieve_colors_from_video(path, show_print = False))
-        row["color_tails"] = str(retrieve_colors_from_video(path, threshold = slice(1, 3), show_print = False))+"\n"+str(retrieve_colors_from_video(path, threshold = slice(None, -4, None), show_print = False))
-        writer.writerow(row)
+def filter_videos(taggings_path):
+    with open(taggings_path, "r") as csvfile:
+        reader = csv.DictReader(csvfile)
+        folder_name = "videos"
+        os.mkdir(folder_name)
+        for row in reader:
+            name = row["name"]
+            path = VIDEOS_PATH+name+".wmv"
+            try:
+                shutil.copyfile(path, folder_name+"/"+name+".wmv")
+            except FileNotFoundError:
+                print(name+" is not found.")
 
-shutil.move(tempfile.name, filename)
+def fill_features(filename):
+    temp_file = NamedTemporaryFile(mode="w", delete=False, newline="")
+    with open(filename, "r") as csvfile, temp_file:
+        reader = csv.DictReader(csvfile)
+        fields = reader.fieldnames
+        writer = csv.DictWriter(temp_file, fieldnames=fields)
+        writer.writeheader()
+        counter = 0
+        for row in reader:
+            counter += 1
+            print(counter)
+            name = row["name"]
+            path = VIDEOS_PATH+name+".wmv"
+            row["color_total"] = str(retrieve_colors_from_video(path, show_print = False))
+            row["color_tails"] = str(retrieve_colors_from_video(path, threshold = slice(1, 3), show_print = False))+"\n"+str(retrieve_colors_from_video(path, threshold = slice(None, -4, None), show_print = False))
+            writer.writerow(row)
+    shutil.move(temp_file.name, filename)
+
+if __name__ == "__main__":
+    filter_videos("tagging_db.csv")
